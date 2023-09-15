3e9dacf7682f67e01bdf920df73cecdbceb23eda
sasidharan.karuppiah <sasidharan.karuppiah@syncfusion.com>
2023-09-15 21:41:46 +0530

    changes

---
+++ angular/default/src/app.component.ts
@@ -0,0 +1,42 @@
+import { Component, ViewEncapsulation, OnInit,ViewChild} from '@angular/core';
+import {
+    PdfViewerComponent, LinkAnnotationService, BookmarkViewService, MagnificationService, ThumbnailViewService,
+    ToolbarService, NavigationService, TextSearchService, TextSelectionService, PrintService, AnnotationService, FormFieldsService, FormDesignerService
+} from '@syncfusion/ej2-angular-pdfviewer';
+import { SwitchComponent } from '@syncfusion/ej2-angular-buttons';
+import { ClickEventArgs } from '@syncfusion/ej2-buttons';
+
+/**
+ * Default PdfViewer Controller
+ */
+@Component({
+    selector: 'app-root',
+    templateUrl: 'app.component.html',
+    encapsulation: ViewEncapsulation.None,
+    // tslint:disable-next-line:max-line-length
+    providers: [LinkAnnotationService, BookmarkViewService, MagnificationService, ThumbnailViewService, ToolbarService, NavigationService, 
+                TextSearchService, TextSelectionService, PrintService, AnnotationService, FormFieldsService, FormDesignerService],
+    styleUrls: ['app.component.css'],
+})
+
+export class AppComponent {
+    @ViewChild('pdfviewer')
+    public pdfviewerControl: PdfViewerComponent;
+    @ViewChild('switch')
+    public switch: SwitchComponent;
+    
+    public document: string = 'https://cdn.syncfusion.com/content/pdf/pdf-succinctly.pdf';
+    ngOnInit(): void {
+        // ngOnInit function
+    }
+    public change(e: any): void {
+        if (e.checked) {
+            this.pdfviewerControl.serviceUrl = '';
+        }
+        else {
+            this.pdfviewerControl.serviceUrl = 'https://ej2services.syncfusion.com/angular/development/api/pdfviewer';
+        }
+        this.pdfviewerControl.dataBind();
+        this.pdfviewerControl.load(this.pdfviewerControl.documentPath, null);
+    }
+}
